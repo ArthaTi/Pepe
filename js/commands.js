54aa9ce581ce733ae3b546eeb45aca05dd4797d4
Soaku <dominik@drozak.net>
2018-04-20 12:16:09 +0200

    Enable syntax-highlighting for edition.

---
+++ js/commands.js
@@ -0,0 +1,31 @@
+var cmds = []
+
+/**
+	Load list of commands
+*/
+function cmd_init() {
+	$.get("../commands.md", function(data){
+		data.replace(/- `([eE]+)`\s*-\s*\[/g, function(m, cmd){
+			cmds.push(cmd);
+		})
+	})
+}
+
+/**
+	Check if the command exists in the latest version of Pepe
+*/
+function cmd_exists(e) {
+	
+	// Remove [Rr] and invalid characters, only E's matter
+	e = e.replace(/[^eE]/g, "");
+	
+	// Commands below 4 length are all available
+	if (e.length < 4) return true;
+	
+	// Characters are all available
+	if (e.length == 9) return true;
+	
+	// If it's on the list, it's available
+	return (cmds.indexOf(e) != -1);
+	
+}
---
+++ server.bat
@@ -0,0 +1,4 @@
+REM |  This file is required to run Pepe in local environment on some browsers.
+REM |  Requires Python 3.x
+REM |  Will start Pepe on localhost:8000
+py -m http.server
